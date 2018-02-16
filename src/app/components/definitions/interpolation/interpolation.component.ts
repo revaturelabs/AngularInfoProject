d429231585a294f3d334b9847eebd3792a13a963
DamonDantin <damondantin@gmail.com>
2018-02-16 13:58:25 -0500

    Finishing interpolation html

--- src/app/components/definitions/interpolation/interpolation.component.ts
+++ src/app/components/definitions/interpolation/interpolation.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./interpolation.component.css']
 })
 export class InterpolationComponent implements OnInit {
-
+    
+    imageInterpolation="https://themarketingtechnologist-ghost.s3.amazonaws.com/2016/Mar/angular-1457385882793.png";
+    
   constructor() { }
 
   ngOnInit() {
