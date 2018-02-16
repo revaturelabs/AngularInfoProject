c1ffad4bd5a54040b151f050c6b44a79edd74d4c
Devin-Dellamano <devin_dellamano@yahoo.com>
2018-02-16 12:28:34 -0500

    Added a Data Types Component. Described the basic data types in TypeScript.

---
+++ src/app/components/typescript/datatypes/datatypes.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datatypes',
+  templateUrl: './datatypes.component.html',
+  styleUrls: ['./datatypes.component.css']
+})
+export class DatatypesComponent implements OnInit {
+  // datatypesimage = 'https://2.bp.blogspot.com/-ycT1q2unjOc/WQbioGK-WWI/AAAAAAAACwU/uJuKxjNWGQUMZDJsyWyDvh5jQc4ErrcqgCLcB/s320/type.png';
+  datatypedocs = 'http://www.typescriptlang.org/docs/handbook/basic-types.html';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
