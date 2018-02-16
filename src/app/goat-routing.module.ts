1c6c574a6b2d02507f1f5cf4b8fff68ae5740153
WinstonRuan <winston.ruan@gmail.com>
2018-02-16 12:10:01 -0500

    created databinding module and goat routing module. changed goat routing module to make merge conflicts

--- src/app/goat-routing.module.ts
+++ src/app/goat-routing.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { DatabindingComponent } from './components/demos/databinding/databinding.component';
+import { DatabindingdefinitionComponent } from './components/definitions/databindingdefinition/databindingdefinition.component';
 
 // Specify Routes of the Application
 export const routes: Routes = [
-
+  { path: 'definitions/databinding', component: DatabindingdefinitionComponent }
 ];
 
 
+// Decorator for a module
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [ RouterModule ]
 })
+
+
 export class GoatRoutingModule { }
