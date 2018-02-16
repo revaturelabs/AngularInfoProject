17325012493b60aa2759ce89fbbbf0b7e6b02832
Ian Gesner <gesner.ian@gmail.com>
2018-02-16 13:25:13 -0500

    finished with feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccessComponent } from './components/typescript/access/access.component';
 import { DatabindingComponent } from './components/demos/databinding/databinding.component';
+import { EnumerationsComponent } from './components/typescript/enumerations/enumerations.component';
 
 // Specify Routes of the Application
 export const routes: Routes = [
     { path: 'intro', component: IntroductionComponent },
     { path: 'access', component: AccessComponent },
-    { path: 'databinding', component: DatabindingComponent}
-
+    { path: 'databinding', component: DatabindingComponent},
+    { path: 'enumerations', component: EnumerationsComponent}
 ];
 
 // Decorator for a module
