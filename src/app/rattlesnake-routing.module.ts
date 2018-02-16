1e5b7c6ebec3b4822d9e06f8c8ac912bf603c18d
Casey Ferris <casey.manning.ferris@gmail.com>
2018-02-16 14:24:04 -0500

    merged for now

--- src/app/rattlesnake-routing.module.ts
+++ src/app/rattlesnake-routing.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DecoratorComponent } from './components/definitions/decorator/decorator.component';
 import { ComponentComponent } from './components/definitions/component/component.component';
+import { InterpolationComponent } from './components/definitions/interpolation/interpolation.component';
 // Specify Routes of the Application
 export const routes: Routes = [
     { path: 'decorator', component: DecoratorComponent },
     { path: 'component', component: ComponentComponent },
-    { path: 'inheritance', component: DefinitionsComponent}
+    { path: 'inheritance', component: DefinitionsComponent},
+    { path: 'interpolation', component: InterpolationComponent}
 
 ];
 
